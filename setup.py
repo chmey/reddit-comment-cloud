2d5cf6463115db8f9534289ae514f7732907c392
chmey <christoph@chmey.com>
2019-09-13 23:58:57 +0200

    Update install_requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setup(
   name='redditcommentcloud',
   packages=['redditcommentcloud'],
-  version='0.1',
+  version='0.1.1',
   license='GPLv3',
   description='Python3.7+ wrapper for generating word clouds from a Reddit user\'s comments.',
   long_description=long_description,
@@ -14,14 +14,14 @@ setup(
   author_email='christoph@chmey.com',
   url='https://github.com/chmey/reddit-comment-cloud',
   keywords=['reddit', 'wordcloud'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'matplotlib',
+          'praw',
+          'WordCloud',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     'Programming Language :: Python :: 3'
   ],
